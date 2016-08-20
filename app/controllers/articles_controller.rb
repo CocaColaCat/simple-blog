d7de567b69892949e082c592b4913c3852dc8d97
jianping zhou <jianping.zhou.1986@gmail.com>
2016-08-20 22:01:35 +0800

    implment comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+ # http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   # 进入创建博文的页面
   def new
     @article = Article.new
